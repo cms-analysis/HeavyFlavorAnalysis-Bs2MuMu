6b67996ea4e766bb084314bf06b289b053adda30
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2010-04-01 14:32:32 +0000

    asd

---
+++ test/test-data/firstLook-XXXX.py
@@ -0,0 +1,75 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HFA")
+
+# ----------------------------------------------------------------------
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+
+# ----------------------------------------------------------------------
+# -- Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "GR10_P_V4::All"
+
+
+# ----------------------------------------------------------------------
+process.source = cms.Source(
+    "PoolSource", 
+    fileNames = cms.untracked.vstring(
+    REPLACEFILES
+    )
+    )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+
+# ----------------------------------------------------------------------
+try:
+    rootFileName = os.environ["JOB"] + ".root"
+except KeyError:
+    rootFileName = "firstLook-XXXX.root"
+
+process.tree = cms.EDAnalyzer(
+    "HFTree",
+    verbose      = cms.untracked.int32(0),
+    requireCand  =  cms.untracked.bool(True),
+    fileName     = cms.untracked.string(rootFileName)
+    )
+
+# ----------------------------------------------------------------------
+#process.load("HeavyFlavorAnalysis.Bs2MuMu.HFMCTruth_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFRecoStuff_cff")
+#process.load("HeavyFlavorAnalysis.Bs2MuMu.HFTruthCandidates_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFB2JpsiCandidates_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFB2MuCandidates_cff")
+#process.load("HeavyFlavorAnalysis.Bs2MuMu.HFBmmCandidates_cff")
+
+#process.load("HeavyFlavorAnalysis.Bs2MuMu.HFMuonAndTrackCandidates_cff")
+#process.load("HeavyFlavorAnalysis.Bs2MuMu.HFDimuonsCandidates_cff")
+
+# ----------------------------------------------------------------------
+process.p = cms.Path(
+#    process.MCTruthSequence*
+    process.recoStuffSequence*
+#    process.bmmDump*
+#    process.truthAllSequence*
+    process.b2D0DsDump*
+    process.b2muD0Dump*
+    process.B2JPsiSequence*
+    process.tree
+)
+
+
+
+
