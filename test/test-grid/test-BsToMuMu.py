e84059c194467727b18a3580fa7a8af6983a716c
Jose Lazo-Flores <sha1-91a8bfb0bc6cbd83cde7fbdf4e103162926f4130@cern.ch>
2010-06-25 13:53:20 +0000

    Updated to new python fragments

--- test/test-grid/test-BsToMuMu.py
+++ test/test-grid/test-BsToMuMu.py
@@ -20,7 +20,7 @@ process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "START3X_V21B::All"
+process.GlobalTag.globaltag = "START3X_V25::All"
 
 
 # ----------------------------------------------------------------------
@@ -132,27 +132,6 @@ process.source = cms.Source(
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 # ----------------------------------------------------------------------
-process.HepPDTESSource = cms.ESSource(
-    "HepPDTESSource",
-    pdtFileName = cms.FileInPath('SimGeneral/HepPDTESSource/data/particle.tbl')
-    )
-process.genParticles = cms.EDProducer(
-    "GenParticleProducer",
-    saveBarCodes          = cms.untracked.bool(True),
-    src                   = cms.InputTag("generator"),
-    abortOnUnknownPDGCode = cms.untracked.bool(False)
-    )
-
-
-# ----------------------------------------------------------------------
-process.genDump = cms.EDAnalyzer(
-    "HFDumpGenerator",
-    generatorCandidates = cms.untracked.string('genParticles'),
-    generatorEvent = cms.untracked.string('generator')
-    )
-
-
-# ----------------------------------------------------------------------
 try:
     rootFileName = os.environ["JOB"] + ".root"
 except KeyError:
@@ -165,202 +144,18 @@ process.tree = cms.EDAnalyzer(
     fileName     = cms.untracked.string(rootFileName)
     )
 
-
 # ----------------------------------------------------------------------
-process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
-process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
-process.trkDump = cms.EDAnalyzer(
-    "HFDumpTracks",
-    verbose                        = cms.untracked.int32(0),
-    tracksLabel                    = cms.untracked.InputTag('generalTracks'),
-    primaryVertexCollectionLabel   = cms.untracked.InputTag('offlinePrimaryVertices'),
-    generatorEventLabel            = cms.untracked.InputTag('generator'),
-    muonsLabel                     = cms.untracked.InputTag("muons"),
-    calomuonsLabel                 = cms.untracked.InputTag("calomuons"),
-    trackingParticlesLabel         = cms.untracked.InputTag('trackingParticles'),
-    associatorLabel                = cms.untracked.InputTag('TrackAssociatorByHits'),
-    doTruthMatching                = cms.untracked.int32(3),
-    simTracksLabel                 = cms.untracked.InputTag('allLayer1TrackCands')
-    )
-
-# ----------------------------------------------------------------------
-process.stuffDump = cms.EDAnalyzer(
-    "HFDumpStuff",
-    verbose                  = cms.untracked.int32(0),
-    PrimaryVertexLabel       = cms.untracked.InputTag("offlinePrimaryVertices"),
-    PrimaryVertexTracksLabel = cms.untracked.InputTag("generalTracks")
-    )
-
-
-# ----------------------------------------------------------------------
-process.muonDump = cms.EDAnalyzer(
-    "HFDumpMuons",
-    verbose         = cms.untracked.int32(0),
-    muonsLabel      = cms.untracked.InputTag("muons"),
-    calomuonsLabel  = cms.untracked.InputTag("calomuons"),
-    doTruthMatching = cms.untracked.int32(0),
-    )
-
-
-# ----------------------------------------------------------------------
-process.triggerDump = cms.EDAnalyzer(
-    "HFDumpTrigger",
-    verbose                 = cms.untracked.int32(0),
-    L1GTReadoutRecordLabel  = cms.untracked.InputTag("gtDigis"), 
-    hltL1GtObjectMap        = cms.untracked.InputTag("hltL1GtObjectMap"), 
-    L1MuonsLabel            = cms.untracked.InputTag("hltL1extraParticles"), 
-    HLTResultsLabel         = cms.untracked.InputTag("TriggerResults::HLT"), 
-    TriggerEventLabel       = cms.untracked.InputTag("hltTriggerSummaryAOD::HLT"), 
-    hltLabel                = cms.untracked.InputTag("TriggerResults::HLT"), 
-    )
-
-# ----------------------------------------------------------------------
-process.bmmDump = cms.EDAnalyzer(
-    "HFDimuons",
-    verbose      = cms.untracked.int32(0), 
-    muonsLabel   = cms.untracked.InputTag("muons"),
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    PrimaryVertexLabel       = cms.untracked.InputTag("offlinePrimaryVertices"),
-    muonPt       = cms.untracked.double(1.0),
-    type         = cms.untracked.int32(1313), 
-    vertexing    = cms.untracked.int32(0), 
-    massLow      = cms.untracked.double(0.5), 
-    massHigh     = cms.untracked.double(12.0)
-    )
-
-
-# ----------------------------------------------------------------------
-process.truthBsToMuMuDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(531),
-    type         = cms.untracked.int32(80),
-    GenType      = cms.untracked.int32(-80),
-    daughtersID  = cms.untracked.vint32(13, -13)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBsToMuMuGaDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(531),
-    type         = cms.untracked.int32(81),
-    GenType      = cms.untracked.int32(-81),
-    daughtersID  = cms.untracked.vint32(13, -13, 22)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBsToKKDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(531),
-    type         = cms.untracked.int32(82),
-    GenType      = cms.untracked.int32(-82),
-    daughtersID  = cms.untracked.vint32(321, -321)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBsToKPiDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(531),
-    type         = cms.untracked.int32(83),
-    GenType      = cms.untracked.int32(-83),
-    daughtersID  = cms.untracked.vint32(321, -211)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBsToPiPiDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(531),
-    type         = cms.untracked.int32(84),
-    GenType      = cms.untracked.int32(-84),
-    daughtersID  = cms.untracked.vint32(211, -211)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBuTo3MuNuDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(521),
-    type         = cms.untracked.int32(70),
-    GenType      = cms.untracked.int32(-70),
-    daughtersID  = cms.untracked.vint32(13, 13, -13, 14)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBdToMuMuDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(511),
-    type         = cms.untracked.int32(90),
-    GenType      = cms.untracked.int32(-90),
-    daughtersID  = cms.untracked.vint32(13, -13)
-    )
-
-# ----------------------------------------------------------------------
-process.truthBdToPiPiDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(511),
-    type         = cms.untracked.int32(91),
-    GenType      = cms.untracked.int32(-91),
-    daughtersID  = cms.untracked.vint32(211, -211)
-    )
-
-# ----------------------------------------------------------------------
-process.truthLambdaBToPPiDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(5122),
-    type         = cms.untracked.int32(60),
-    GenType      = cms.untracked.int32(-60),
-    daughtersID  = cms.untracked.vint32(2212, -211)
-    )
-
-# ----------------------------------------------------------------------
-process.truthLambdaBToPKDump = cms.EDAnalyzer(
-    "HFTruthCandidate",
-    verbose      = cms.untracked.int32(0), 
-    tracksLabel  = cms.untracked.InputTag('generalTracks'),
-    motherID     = cms.untracked.int32(5122),
-    type         = cms.untracked.int32(61),
-    GenType      = cms.untracked.int32(-61),
-    daughtersID  = cms.untracked.vint32(2212, -321)
-    )
-
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFMCTruth_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFRecoStuff_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFTruthCandidates_cff")
+#process.load("HeavyFlavorAnalysis.Bs2MuMu.HFB2JpsiCandidates_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFBmmCandidates_cff")
 # ----------------------------------------------------------------------
 process.p = cms.Path(
-    process.genParticles* 
-    process.genDump*
-    process.stuffDump*
-    process.trkDump*
-    process.muonDump*
-    process.triggerDump*
-    process.bmmDump*
+    process.MCTruthSequence*
+    process.recoStuffSequence*
     process.truthBsToMuMuDump*
-    process.truthBdToMuMuDump*
-    process.truthBsToKKDump*
-    process.truthBsToKPiDump*
-    process.truthBdToPiPiDump*
-    process.truthBsToPiPiDump*
-    process.truthLambdaBToPPiDump*
-    process.truthLambdaBToPKDump*
-    process.truthBuTo3MuNuDump*
-    process.truthBsToMuMuGaDump*
+    process.bmmDump*
     process.tree
 )
-
-
-
 
