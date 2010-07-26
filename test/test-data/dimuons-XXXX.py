f89e030a4c4115095c20615b3b261e3d7acc8286
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2010-07-26 12:37:44 +0000

    changed mkPyFiles, tested for -r

---
+++ test/test-data/dimuons-XXXX.py
@@ -0,0 +1,63 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HFA")
+
+# ----------------------------------------------------------------------
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.categories.append('HLTrigReport')
+process.MessageLogger.categories.append('L1GtTrigReport')
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+
+# ----------------------------------------------------------------------
+# -- Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "GR10_P_V4::All"
+
+
+# ----------------------------------------------------------------------
+# POOLSOURCE
+
+# ----------------------------------------------------------------------
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFTree_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFRecoStuff_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFTruthCandidates_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFDimuonsCandidates_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFPhysicsDeclared_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFSkipEvents_cff")
+
+
+# ----------------------------------------------------------------------
+try:
+    rootFileName = os.environ["JOB"] + ".root"
+except KeyError:
+    rootFileName = "dimuons-XXXX.root"
+
+process.tree.fileName = rootFileName
+
+
+# ----------------------------------------------------------------------
+process.p = cms.Path(
+    process.seqPhysDeclBitSelection*
+    process.skipEvents*
+#    process.MCTruthSequence*
+    process.recoStuffSequence*
+#    process.truthAllSequence*
+#    process.mtDump*
+    process.dimuonsDump* 
+#    process.B2JPsiSequence*
+#    process.HFCharmSequence*
+#    process.HFMuCharmDump*
+    process.tree
+)
+
