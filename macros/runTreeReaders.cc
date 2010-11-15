7384998a1546c564cfb5bd9070ac4538f6437c69
Christoph Nageli <sha1-067387c955e77eaf9ce2d72cbd8fd47dbe7f524e@cern.ch>
2010-11-15 09:43:10 +0000

    added the new readers to runTreeReaders

--- macros/runTreeReaders.cc
+++ macros/runTreeReaders.cc
@@ -28,6 +28,8 @@
 #include "genLevel.hh"
 #include "decayCounter.hh"
 #include "impactReader.hh"
+#include "kpIsolationReader.hh"
+#include "phiIsolationReader.hh"
 
 using namespace std;
 
@@ -196,6 +198,8 @@ int main(int argc, char *argv[]) {
   else if (readerName == "genLevel") a = new genLevel(chain,TString(evtClassName));
   else if (readerName == "decayCounter") a = new decayCounter(chain,TString(evtClassName));
   else if (readerName == "impactReader") a = new impactReader(chain,TString(evtClassName));
+  else if (readerName == "kpIsolationReader") a = new kpIsolationReader(chain,TString(evtClassName));
+  else if (readerName == "phiIsolationReader") a = new phiIsolationReader(chain,TString(evtClassName));
   else {
     cout << "please provide a class name to instantiate" << endl;
   }
