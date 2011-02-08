9e2697c2d13600ea9b4cb3f5009495f6c8563186
Frank Meier <sha1-cbef2ad636c62802bd9545d5d36a8875981b19b2@cern.ch>
2011-02-08 11:10:23 +0000

    We can now use the V0collection from the V0producer in AOD/RECO as well. Added some cuts to control size of combinatorics. Reformatted using astyle -A1, so better use cvs diff -b to supress changed in whitespace.

--- python/HFLambdas_cff.py
+++ python/HFLambdas_cff.py
@@ -7,21 +7,40 @@ HFLambdasDump = cms.EDAnalyzer(
     tracksLabel        = cms.untracked.InputTag('generalTracks'),
     PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
     muonsLabel         = cms.untracked.InputTag("muons"),
-    muonPt             = cms.untracked.double(1.0),
-    pionPt             = cms.untracked.double(0.7),
-    protonPt           = cms.untracked.double(0.7),
+    muonPt             = cms.untracked.double(0.7),
+    pionPt             = cms.untracked.double(0.1),
+    protonPt           = cms.untracked.double(0.2),
     psiMuons	       = cms.untracked.int32(2),
     psiWindow	       = cms.untracked.double(0.3),
     L0Window           = cms.untracked.double(0.3),
     ksWindow	       = cms.untracked.double(0.3),
     LbWindow           = cms.untracked.double(0.8),
     deltaR             = cms.untracked.double(99.),
-    maxDoca            = cms.untracked.double(0.1),
-    pAngle	       = cms.untracked.double(0.1)
-    )
+    maxDoca            = cms.untracked.double(0.5),
+    pAngle	       = cms.untracked.double(0.1),
+    useV0              = cms.untracked.bool(False)
+)
+
+HFLambdasDumpV0 = cms.EDAnalyzer(
+    "HFLambdas",
+    verbose            = cms.untracked.int32(0), 
+    tracksLabel        = cms.untracked.InputTag('generalTracks'),
+    PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
+    muonsLabel         = cms.untracked.InputTag("muons"),
+    muonPt             = cms.untracked.double(0.7),
+    pionPt             = cms.untracked.double(0.1),
+    protonPt           = cms.untracked.double(0.2),
+    psiMuons	       = cms.untracked.int32(2),
+    psiWindow	       = cms.untracked.double(0.3),
+    ksWindow	       = cms.untracked.double(0.3),
+    LbWindow           = cms.untracked.double(0.8),
+    deltaR             = cms.untracked.double(99.),
+    pAngle	       = cms.untracked.double(0.1),
+    useV0              = cms.untracked.bool(True)
+)
 
 # ######################################################################
 # Sequences
 # ######################################################################
-HFLambdasSequence     = cms.Sequence(HFLambdasDump)
+HFLambdasSequence     = cms.Sequence(HFLambdasDump*HFLambdasDumpV0)
 
