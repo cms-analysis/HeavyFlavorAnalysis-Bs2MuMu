613f2e40fb1a40d10da92aaf1e821c774c2cc797
Danek Kotlinski <sha1-3a033e6407e48eb9fbe364eec828608ee1ba03ae@cern.ch>
2012-05-09 09:50:42 +0000

    Add the di-track sequence

--- python/HFBmm_cff.py
+++ python/HFBmm_cff.py
@@ -54,7 +54,25 @@ bspsiphiDump = cms.EDAnalyzer(
     maxDz              = cms.untracked.double(99.0)
     )
 
+# ----------------------------------------------------------------------
+hhDump = cms.EDAnalyzer(
+    "HFDiTracks",
+    verbose            = cms.untracked.int32(0), 
+    tracksLabel        = cms.untracked.InputTag("generalTracks"),
+    PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
+    trackPt            = cms.untracked.double(4.0),
+    track1Mass         = cms.untracked.double(0.1057),
+    track2Mass         = cms.untracked.double(0.1057),
+    massLow            = cms.untracked.double(4.5),
+    massHigh           = cms.untracked.double(6.5),
+    maxDoca            = cms.untracked.double(0.025),
+    pvWeight           = cms.untracked.double(0.70),
+    type              = cms.untracked.int32(211211)
+    )
+
+
 # ######################################################################
 # Sequences
 # ######################################################################
-bmmSequence     = cms.Sequence(bmmDump*bupsikpDump*bspsiphiDump)
+#bmmSequence_old     = cms.Sequence(bmmDump*bupsikpDump*bspsiphiDump)
+bmmSequence     = cms.Sequence(bmmDump*bupsikpDump*bspsiphiDump*hhDump)
