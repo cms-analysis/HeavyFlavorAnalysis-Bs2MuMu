45bf33fa101a5124799ffb600139d9560b7e518a
Jose Lazo-Flores <sha1-91a8bfb0bc6cbd83cde7fbdf4e103162926f4130@cern.ch>
2012-05-10 14:27:43 +0000

    Made changes to fix some bugs.

--- mclimit/prepUprLimCalc.h
+++ mclimit/prepUprLimCalc.h
@@ -32,7 +32,7 @@ char *ename[NSYS];
 char accname[]="ACC";// acceptance
 char pdfhadron[]="HPDF";
 char toteffname[]="TOTEFF";
-char bkgerrname[]="BKGERR";
+char rarebkgerrname[]="BKGERR";
 char combbkgerrname[]="CBKGERR";
 char experrname[]="EXPERR";
 char unconstrned[]="UNCONSTRAINED";
@@ -202,7 +202,7 @@ if (cms11bs) {
 		// Add rare background templates
 		sfact = 1;
 		
-		ename[0] = bkgerrname;
+		ename[0] = rarebkgerrname;
 		nps_low[0] = -rarebkgbswinerr/rarebkgbswin;
 		nps_high[0] = rarebkgbswinerr/rarebkgbswin;
 		
@@ -369,7 +369,7 @@ if (cms11bs) {
 		// Add rare background templates
 		sfact = 1;
 		
-		ename[0] = bkgerrname;
+		ename[0] = rarebkgerrname;
 		nps_low[0] = -rarebkgbswinerr/rarebkgbswin;
 		nps_high[0] = rarebkgbswinerr/rarebkgbswin;
 		
@@ -534,7 +534,7 @@ if (cms11bd) {
 		// Add rare background templates
 		sfact = 1;
 		
-		ename[0] = bkgerrname;
+		ename[0] = rarebkgerrname;
 		nps_low[0] = -rarebkgbdwinerr/rarebkgbdwin;
 		nps_high[0] = rarebkgbdwinerr/rarebkgbdwin;
 		
@@ -701,7 +701,7 @@ if (cms11bd) {
 		// Add rare background templates
 		sfact = 1;
 		
-		ename[0] = bkgerrname;
+		ename[0] = rarebkgerrname;
 		nps_low[0] = -rarebkgbdwinerr/rarebkgbdwin;
 		nps_high[0] = rarebkgbdwinerr/rarebkgbdwin;
 		
@@ -849,7 +849,8 @@ if (cms12bs) {
 		cout << "Observed in Bs window = " << obsrvbswin << ", in Bd window = " << obsrvbdwin << endl << endl;
 		
 		Double_t rarebkgbdwin = h_bkgB->Integral(lobldbin,lobsmssbin); //Double_t rarebkgbdwinerr = 0.070;
-		Double_t rarebkgbswin = h_bkgB->Integral(lobsmssbin,hibldbin); Double_t rarebkgbswinerr = 0.060;
+//		Double_t rarebkgbswin = h_bkgB->Integral(lobsmssbin,hibldbin); Double_t rarebkgbswinerr = 0.060;
+		Double_t rarebkgbswin = 0.18; Double_t rarebkgbswinerr = 0.060;
 		Double_t rarebkgleft = h_bkgB->Integral(lowmassbin,lobldbin); Double_t rarebkgright = h_bkgB->Integral(hibldbin,himassbin);
 		Double_t rarebkgshldrs = rarebkgleft + rarebkgright; Double_t rarebkgshldrserr = 0.632963;
 		cout << "Rare bkgd (shoulders) = " << rarebkgshldrs << endl;
@@ -858,14 +859,16 @@ if (cms12bs) {
 		
 		Double_t combkg = (obsrvshldrs - rarebkgshldrs);
 		Double_t combkgbd = (obsrvshldrs - rarebkgleft)*bdmsswin/msswin; Double_t combkgbderr = 0.34;
-		Double_t combkgbs = (obsrvshldrs - rarebkgleft)*bsmsswin/msswin; Double_t combkgbserr = 0.50;
+//		Double_t combkgbs = (obsrvshldrs - rarebkgleft)*bsmsswin/msswin; Double_t combkgbserr = 0.50;
+		Double_t combkgbs = 0.59; Double_t combkgbserr = 0.50;
 		cout << "CombBkgd (shoulders) = " << combkg << endl;
 		cout << "CombBkgd (in Bd blind window) = " << combkgbd << " +- " << combkgbderr << endl;
 		cout << "CombBkgd (in Bs blind window) = " << combkgbs << " +- " << combkgbserr  << endl << endl;
 		
 		Double_t exptbdevnts = h_sigbdB->Integral(lobldbin,lobsmssbin); Double_t exptbdevntserr = 0.020;
 		Double_t exptbdevntsbswin = h_sigbdB->Integral(lobsmssbin,hibldbin);
-		Double_t exptbsevnts = h_sigbsB->Integral(lobsmssbin,hibldbin); Double_t exptbsevntserr = 0.41;
+//		Double_t exptbsevnts = h_sigbsB->Integral(lobsmssbin,hibldbin); Double_t exptbsevntserr = 0.41;
+		Double_t exptbsevnts = 2.70; Double_t exptbsevntserr = 0.41;
 		Double_t exptbsevntsbdwin = h_sigbsB->Integral(lobldbin,lobsmssbin);
 		cout << "Expected Bd signal events = " << exptbdevnts << " +- " << exptbdevntserr << endl;
 		cout << "Expected Bd signal events in Bs window = " << exptbdevntsbswin << endl;	
@@ -903,6 +906,7 @@ if (cms12bs) {
 		cout << "<<<<<< Done Setting up Histos for Barrel Channel>>>>>>>" << endl;
 		
 		// Initialize everything
+		// Add rare background templates
 		for(int i=0;i<NSYS;i++)
 		{
 			nps_low[i]  = 0;
@@ -913,17 +917,15 @@ if (cms12bs) {
 			highshape[i]= 0;
 		}
 		
-		// Add rare background templates
 		sfact = 1;
-		ename[0] = bkgerrname;
-		nps_low[0] = -rarebkgshldrserr/rarebkgshldrs;
-		nps_high[0] = rarebkgshldrserr/rarebkgshldrs;
+		ename[0] = rarebkgerrname;
+		nps_low[0] = -rarebkgbswinerr/rarebkgbswin;
+		nps_high[0] = rarebkgbswinerr/rarebkgbswin;
+//		nps_low[0] = -rarebkgshldrserr/rarebkgshldrs;
+//		nps_high[0] = rarebkgshldrserr/rarebkgshldrs;
 		
-//		ename[1] = psdname;
-//		nps_low[1] = -0.0224451/0.291961;
-//		nps_high[1] = 0.0224451/0.291961;
 		
-		nps_count=1;	
+		nps_count = 1;	
 		
 		pssnflg = 0;
 		sclflg = 0;
@@ -953,11 +955,11 @@ if (cms12bs) {
 		}
 		
 		sfact = 1;
-		ename[0] = bkgerrname;
-		nps_low[1] = 0.0;
-		nps_high[1] = 0.0;
+		ename[0] = combbkgerrname;
+		nps_low[0] = -combkgbserr/combkgbs;
+		nps_high[0] = combkgbserr/combkgbs;
 		
-		nps_count=1;
+		nps_count = 1;
 		
 		pssnflg = 0;
 		sclflg = 0;
@@ -974,40 +976,7 @@ if (cms12bs) {
 							  lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameB);
 		cout << "Finished setting up nullhyp, testhyp, nullhyp_pe, testhyp_pe for combinatorial background" << endl;
 		
-		
-		// Add Bd background contribution templates
-		for(int i=0;i<NSYS;i++)
-		{
-			nps_low[i]  = 0;
-			nps_high[i] = 0;
-			lowsigma[i] = 0;
-			highsigma[i]= 0;
-			lowshape[i] = 0;
-			highshape[i]= 0;
-		}
-		
-		sfact = 1;
-		ename[0] = unconstrned;
-		nps_low[0] = -0.0224451/0.291961;
-		nps_high[0] = 0.0224451/0.291961;
-		
-		nps_count=0;
-		
-		pssnflg = 0;
-		sclflg = 0;
-		
-		// Construct test/null hypothesis for pseudo-experiments.
-		nullhyp_pe->add_template(h_signalbdB,sfact,nps_count,ename,nps_low,nps_high,
-								 lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameB);
-		testhyp_pe->add_template(h_signalbdB,sfact,nps_count,ename,nps_low,nps_high,
-								 lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameB);
-		nps_count=0;
-		nullhyp->add_template(h_signalbdB,sfact,nps_count,ename,nps_low,nps_high,
-							  lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameB);
-		testhyp->add_template(h_signalbdB,sfact,nps_count,ename,nps_low,nps_high,
-							  lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameB);
-		cout << "Finished setting up nullhyp, testhyp, nullhyp_pe, testhyp_pe for Bd background" << endl;
-		
+				
 		// Add signal templates
 		for(int i=0;i<NSYS;i++) {
 			nps_low[i] = 0;
@@ -1019,30 +988,36 @@ if (cms12bs) {
 		}
 		
 		ename[0] = pdfhadron;
-		nps_low[0] = -0.021/0.267;
-		nps_high[0] = 0.021/0.267;
+//		nps_low[0] = -0.021/0.267;
+//		nps_high[0] = 0.021/0.267;
+//		nps_low[0] = -0.1192/3.5487;
+//		nps_high[0] = 0.1192/3.5487;
+		nps_low[0] = -0.2958/3.745;
+		nps_high[0] = 0.2958/3.745;
 		
 		ename[1] = toteffname;
 		nps_low[1] = -totefferr/toteff;
 		nps_high[1] = totefferr/toteff;
 		
 		ename[2] = experrname;
-		nps_low[2] = -totbswinerr/totbswin;
-		nps_high[2] = totbswinerr/totbswin;
+//		nps_low[2] = -totbswinerr/totbswin;
+//		nps_high[2] = totbswinerr/totbswin;
+		nps_low[2] = -exptbsevntserr/exptbsevnts;
+		nps_high[2] = exptbsevntserr/exptbsevnts;
 		
-		ename[3] = accname;
-		nps_low[3] = -0.009/0.248;
-		nps_high[3] = 0.009/0.248;
+		ename[3] = rarebkgerrname;
+		nps_low[3] = -rarebkgbswinerr/rarebkgbswin;
+		nps_high[3] = rarebkgbswinerr/rarebkgbswin;
+		//		
+		ename[4] = combbkgerrname;
+		nps_low[4] = -combkgbserr/combkgbs;
+		nps_high[4] = combkgbserr/combkgbs;
+		//		
+//		ename[5] = peakerrname;
+//		nps_low[5] = -rarebkgbswinerr/rarebkgbswin;
+//		nps_high[5] = rarebkgbswinerr/rarebkgbswin;
 		
-		ename[4] = pssname;
-		nps_low[4] = -0.0439992/0.862683;
-		nps_high[4] = 0.0439992/0.862683;
-		
-		ename[5] = peakerrname;
-		nps_low[5] = -rarebkgbswinerr/rarebkgbswin;
-		nps_high[5] = rarebkgbswinerr/rarebkgbswin;
-		
-		nps_count = 6;		
+		nps_count = 3;		
 		sfact = 1.;
 		
 		pssnflg = 0;// 
@@ -1069,7 +1044,8 @@ if (cms12bs) {
 		cout << "Observed in Bs window = " << obsrvbswin << ", in Bd window = " << obsrvbdwin << endl << endl;
 		
 		Double_t rarebkgbdwin = h_bkgE->Integral(lobldbin,lobsmssbin); //Double_t rarebkgbdwinerr = 0.030;
-		Double_t rarebkgbswin = h_bkgE->Integral(lobsmssbin,hibldbin); Double_t rarebkgbswinerr = 0.020;
+//		Double_t rarebkgbswin = h_bkgE->Integral(lobsmssbin,hibldbin); Double_t rarebkgbswinerr = 0.020;
+		Double_t rarebkgbswin = 0.08; Double_t rarebkgbswinerr = 0.020;
 		Double_t rarebkgleft = h_bkgE->Integral(lowmassbin,lobldbin); Double_t rarebkgright = h_bkgE->Integral(hibldbin,himassbin);
 		Double_t rarebkgshldrs = rarebkgleft + rarebkgright; Double_t rarebkgshldrserr = 0.238872;
 		cout << "Rare bkgd (shoulders) = " << rarebkgshldrs << endl;
@@ -1078,23 +1054,25 @@ if (cms12bs) {
 		
 		Double_t combkg = (obsrvshldrs - rarebkgshldrs);
 		Double_t combkgbd = (obsrvshldrs - rarebkgleft)*bdmsswin/msswin; Double_t combkgbderr = 0.35;
-		Double_t combkgbs = (obsrvshldrs - rarebkgleft)*bsmsswin/msswin; Double_t combkgbserr = 0.53;
+//		Double_t combkgbs = (obsrvshldrs - rarebkgleft)*bsmsswin/msswin; Double_t combkgbserr = 0.53;
+		Double_t combkgbs = 1.14; Double_t combkgbserr = 0.53;
 		cout << "CombBkgd (shoulders) = " << combkg << endl;
 		cout << "CombBkgd (in Bd blind window) = " << combkgbd << " +- " << combkgbderr << endl;
 		cout << "CombBkgd (in Bs blind window) = " << combkgbs << "+- " << combkgbserr << endl << endl;
 		
 		Double_t exptbdevnts = h_sigbdE->Integral(lobldbin,lobsmssbin); Double_t exptbdevntserr = 0.010;
 		Double_t exptbdevntsbswin = h_sigbdE->Integral(lobsmssbin,hibldbin);
-		Double_t exptbsevnts = h_sigbsE->Integral(lobsmssbin,hibldbin); Double_t exptbsevntserr = 0.18;
+//		Double_t exptbsevnts = h_sigbsE->Integral(lobsmssbin,hibldbin); Double_t exptbsevntserr = 0.18;
+		Double_t exptbsevnts = 1.23; Double_t exptbsevntserr = 0.18;
 		Double_t exptbsevntsbdwin = h_sigbsE->Integral(lobldbin,lobsmssbin);
 		cout << "Expected Bd signal events = " << exptbdevnts << " +- " << exptbdevntserr << endl;
 		cout << "Expected Bd signal events in Bs window = " << exptbdevntsbswin << endl;	
 		cout << "Expected Bs signal events = " << exptbsevnts << " +- " << exptbsevntserr<< endl;
 		cout << "Expected Bs signal events in Bd window = " << exptbsevntsbdwin << endl << endl;
 		
-		Double_t totbdwin = exptbdevnts + combkgbd + rarebkgbdwin + exptbsevntsbdwin;// including exptbsevntsbdwin
+		Double_t totbdwin = exptbdevnts + combkgbd + rarebkgbdwin;// + exptbsevntsbdwin;// including exptbsevntsbdwin
 		//Double_t totbdwinerr = 0.35;
-		Double_t totbswin = exptbsevnts + combkgbs + rarebkgbswin + exptbdevntsbswin;// including exptbsevntsbswin
+		Double_t totbswin = exptbsevnts + combkgbs + rarebkgbswin;// + exptbdevntsbswin;// including exptbsevntsbswin
 		Double_t totbswinerr = 0.56;
 		Double_t totblndwin = totbswin + totbdwin;
 		cout << "Total (expected in Bd blind window) = " << totbdwin << " ~ " << floor(totbdwin + 0.5) << endl;
@@ -1123,6 +1101,7 @@ if (cms12bs) {
 		cout << "<<<<<< Done Setting up Histos for Endcap Channel>>>>>>>" << endl;
 		
 		// Initialize everything
+		// Add rare background templates
 		for(int i=0;i<NSYS;i++)
 		{
 			nps_low[i]  = 0;
@@ -1133,17 +1112,15 @@ if (cms12bs) {
 			highshape[i]= 0;
 		}
 		
-		// Add rare background templates
 		sfact = 1;
-		ename[0] = bkgerrname;
-		nps_low[0] = -rarebkgshldrserr/rarebkgshldrs;
-		nps_high[0] = rarebkgshldrserr/rarebkgshldrs;
+		ename[0] = rarebkgerrname;
+		nps_low[0] = -rarebkgbswinerr/rarebkgbswin;
+		nps_high[0] = rarebkgbswinerr/rarebkgbswin;
+//		nps_low[0] = -rarebkgshldrserr/rarebkgshldrs;
+//		nps_high[0] = rarebkgshldrserr/rarebkgshldrs;
 		
-//		ename[1] = psdname;
-//		nps_low[1] = -0.0285105/0.318653;
-//		nps_high[1] = 0.0285105/0.318653;
 		
-		nps_count=1;	
+		nps_count = 1;	
 		
 		pssnflg = 0;
 		sclflg = 0;
@@ -1172,11 +1149,11 @@ if (cms12bs) {
 		}
 		
 		sfact = 1;
-		ename[0] = bkgerrname;
-		nps_low[0] = 0.;
-		nps_high[0] = 0.;
+		ename[0] = combbkgerrname;
+		nps_low[0] = -combkgbserr/combkgbs;
+		nps_high[0] = combkgbserr/combkgbs;
 		
-		nps_count=0;
+		nps_count = 1;
 		
 		pssnflg = 0;
 		sclflg = 0;
@@ -1192,40 +1169,7 @@ if (cms12bs) {
 		testhyp->add_template(h_combbkgE,sfact,nps_count,ename,nps_low,nps_high,
 							  lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameE);
 		cout << "Finished setting up nullhyp, testhyp, nullhyp_pe, testhyp_pe for combinatorial background" << endl;
-		
-		// Add Bd background contribution templates
-		for(int i=0;i<NSYS;i++)
-		{
-			nps_low[i]  = 0;
-			nps_high[i] = 0;
-			lowsigma[i] = 0;
-			highsigma[i]= 0;
-			lowshape[i] = 0;
-			highshape[i]= 0;
-		}
-		
-		sfact = 1;
-		ename[0] = unconstrned;
-		nps_low[0] = -0.0285105/0.318653;
-		nps_high[0] = 0.0285105/0.318653;
-		
-		nps_count=1;
-		
-		pssnflg = 0;
-		sclflg = 0;
-		
-		// Construct test/null hypothesis for pseudo-experiments.
-		nullhyp_pe->add_template(h_signalbdE,sfact,nps_count,ename,nps_low,nps_high,
-								 lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameE);
-		testhyp_pe->add_template(h_signalbdE,sfact,nps_count,ename,nps_low,nps_high,
-								 lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameE);
-		nps_count=0;
-		nullhyp->add_template(h_signalbdE,sfact,nps_count,ename,nps_low,nps_high,
-							  lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameE);
-		testhyp->add_template(h_signalbdE,sfact,nps_count,ename,nps_low,nps_high,
-							  lowshape,lowsigma,highshape,highsigma,pssnflg,sclflg,channameE);
-		cout << "Finished setting up nullhyp, testhyp, nullhyp_pe, testhyp_pe for Bd background" << endl;
-		
+				
 		
 		// Add signal templates
 		for(int i=0;i<NSYS;i++) {
@@ -1238,32 +1182,36 @@ if (cms12bs) {
 		}
 		
 		ename[0] = pdfhadron;
-		nps_low[0] = -0.021/0.267;
-		nps_high[0] = 0.021/0.267;
-		//		nps_low[0] = -0.1192/3.5487;
-		//		nps_high[0] = 0.1192/3.5487;
+//		nps_low[0] = -0.021/0.267;
+//		nps_high[0] = 0.021/0.267;
+//		nps_low[0] = -0.1192/3.5487;
+//		nps_high[0] = 0.1192/3.5487;
+		nps_low[0] = -0.2958/3.745;
+		nps_high[0] = 0.2958/3.745;
 		
 		ename[1] = toteffname;
 		nps_low[1] = -totefferr/toteff;
 		nps_high[1] = totefferr/toteff;
 		
 		ename[2] = experrname;
-		nps_low[2] = -totbswinerr/totbswin;
-		nps_high[2] = totbswinerr/totbswin;
+//		nps_low[2] = -totbswinerr/totbswin;
+//		nps_high[2] = totbswinerr/totbswin;
+		nps_low[2] = -exptbsevntserr/exptbsevnts;
+		nps_high[2] = exptbsevntserr/exptbsevnts;
 		
-		ename[3] = accname;
-		nps_low[3] = -0.011/0.229;
-		nps_high[3] = 0.011/0.229;
+		ename[3] = rarebkgerrname;
+		nps_low[3] = -rarebkgbswinerr/rarebkgbswin;
+		nps_high[3] = rarebkgbswinerr/rarebkgbswin;
+		//		
+		ename[4] = combbkgerrname;
+		nps_low[4] = -combkgbserr/combkgbs;
+		nps_high[4] = combkgbserr/combkgbs;
+		//		
+//		ename[5] = peakerrname;
+//		nps_low[5] = -rarebkgbswinerr/rarebkgbswin;
+//		nps_high[5] = rarebkgbswinerr/rarebkgbswin;
 		
-		ename[4] = pssname;
-		nps_low[4] = -0.0387952/0.714124;
-		nps_high[4] = 0.0387952/0.714124;
-		
-		ename[5] = peakerrname;
-		nps_low[5] = -rarebkgbswinerr/rarebkgbswin;
-		nps_high[5] = rarebkgbswinerr/rarebkgbswin;
-		
-		nps_count = 6;
+		nps_count = 3;
 		
 		sfact = 1.;
 		
@@ -1393,7 +1341,7 @@ if (cms12bd) {
 		
 		// Add rare background templates
 		sfact = 1;
-		ename[0] = bkgerrname;
+		ename[0] = rarebkgerrname;
 		nps_low[0] = -rarebkgshldrserr/rarebkgshldrs;
 		nps_high[0] = rarebkgshldrserr/rarebkgshldrs;
 		
@@ -1431,7 +1379,7 @@ if (cms12bd) {
 		}
 		
 		sfact = 1;
-		ename[0] = bkgerrname;
+		ename[0] = combbkgerrname;
 		nps_low[0] = 0;
 		nps_high[0] = 0;
 		
@@ -1607,7 +1555,7 @@ if (cms12bd) {
 		
 		// Add rare background templates
 		sfact = 1;
-		ename[0] = bkgerrname;
+		ename[0] = rarebkgerrname;
 		nps_low[0] = -rarebkgshldrserr/rarebkgshldrs;
 		nps_high[0] = rarebkgshldrserr/rarebkgshldrs;
 		
@@ -1644,7 +1592,7 @@ if (cms12bd) {
 		}
 		
 		sfact = 1;
-		ename[0] = bkgerrname;
+		ename[0] = combbkgerrname;
 		nps_low[0] = 0.;
 		nps_high[0] = 0.;
 		
