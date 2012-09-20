ac196ceebe1efc360c865832921ce84392be8383
Christoph Nageli <sha1-067387c955e77eaf9ce2d72cbd8fd47dbe7f524e@cern.ch>
2012-09-20 14:38:49 +0000

    - Work with Storage Element - Also merge SM computation

--- ulcalc/python/merge_files.py
+++ ulcalc/python/merge_files.py
@@ -18,19 +18,30 @@ using namespace RooStats;
 void merge(const char *filename, const char *resultname, const char *fromfile)
 {
 	TFile theFile(filename,"update");
-	TFile fromF(fromfile);
+	TFile *fromF = TFile::Open(fromfile);
+	std::string resultnameSM(Form("%s_SM",resultname));
 	
 	RooStats::HypoTestInverterResult *result = dynamic_cast<RooStats::HypoTestInverterResult*>(theFile.Get(resultname));
 	if(!result) // not yet loaded
-	  result = new RooStats::HypoTestInverterResult(resultname);
+	  result = new RooStats::HypoTestInverterResult(resultname);          
+        RooStats::HypoTestInverterResult *resultSM = dynamic_cast<RooStats::HypoTestInverterResult*>(theFile.Get(resultnameSM.c_str()));
+	if(!resultSM)
+	  resultSM = new RooStats::HypoTestInverterResult(resultnameSM.c_str());
 
-	RooWorkspace *wspace = dynamic_cast<RooWorkspace*> (fromF.Get("wspace"));
-	RooStats::HypoTestInverterResult *toadd = dynamic_cast<RooStats::HypoTestInverterResult*> (wspace->obj(resultname));
-	
+	RooWorkspace *wspace = dynamic_cast<RooWorkspace*> (fromF->Get("wspace"));
+
+	RooStats::HypoTestInverterResult *toadd = dynamic_cast<RooStats::HypoTestInverterResult*> (wspace->obj(resultname));	
 	result->Add(*toadd);
+
+	toadd = dynamic_cast<RooStats::HypoTestInverterResult*>(wspace->obj(resultnameSM.c_str()));
+	resultSM->Add(*toadd);
+
 	theFile.cd();
 	result->Write(resultname,TObject::kOverwrite);
+	resultSM->Write(resultnameSM.c_str(),TObject::kOverwrite);
 	theFile.Close();
+
+        delete fromF;
 } // merge_result()
 '''
 
