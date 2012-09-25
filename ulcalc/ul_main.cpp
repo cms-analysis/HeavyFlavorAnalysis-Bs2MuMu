e09ef89bf7b34d3275736ed505fc4fba30f4ae8d
Christoph Nageli <sha1-067387c955e77eaf9ce2d72cbd8fd47dbe7f524e@cern.ch>
2012-09-25 14:21:06 +0000

    write same thing into log file as to stdout

--- ulcalc/ul_main.cpp
+++ ulcalc/ul_main.cpp
@@ -538,7 +538,7 @@ int main(int argc, const char *argv [])
 	if (output_path) {
 		FILE *outFile = fopen(output_path, "w");
 		if (gAlgorithm == kAlgo_CLb || gAlgorithm == kAlgo_CLb_Hybrid) {
-			fprintf(outFile, "p value of background model: %.3f\n corresponding to %2.1f sigmas\n", upperLimit, sqrt(2.)*TMath::ErfInverse(1 - 2*upperLimit));
+			fprintf(outFile, "p value of background model: %f\n corresponding to %f sigmas\n", upperLimit, sqrt(2.)*TMath::ErfInverse(1 - 2*upperLimit));
 		} else {
 			for (ch = channels.begin(); ch != channels.end(); ++ch) {
 				fprintf(outFile, "NbObs_%d=%d, NsObs_%d=%d, NdObs_%d=%d\n", *ch, (int)(bsmm[make_pair(kObsBkg_bmm, *ch)].getVal()), *ch, (int)(bsmm[make_pair(kObsB_bmm, *ch)].getVal()),*ch, (int)(bdmm[make_pair(kObsB_bmm, *ch)].getVal()));
