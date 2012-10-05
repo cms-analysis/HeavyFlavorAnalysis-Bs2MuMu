4c08027269f3c21431602865c05028622484e090
Christoph Nageli <sha1-067387c955e77eaf9ce2d72cbd8fd47dbe7f524e@cern.ch>
2012-10-05 09:40:48 +0000

    work with two sided limits

--- ulcalc/scripts/plotResult.cpp
+++ ulcalc/scripts/plotResult.cpp
@@ -12,12 +12,14 @@
 #include <TLegend.h>
 #include <TGraphAsymmErrors.h>
 #include <TMultiGraph.h>
+#include <TVirtualPad.h>
 #include <RooNumber.h>
 #include <RooWorkspace.h>
 
 #include <RooStats/SamplingDistribution.h>
 #include <RooStats/HypoTestInverterResult.h>
 
+// upper limit strings
 #define kVDEF_ulObs			"default-11:ulObs:val"
 #define kVDEF_ulBkgMed		"default-11:ulBkg:val"
 #define kVDEF_ulBkgErrHi	"default-11:ulBkg:errHi"
@@ -26,6 +28,14 @@
 #define kVDEF_ulSMErrHi		"default-11:ulSM:errHi"
 #define kVDEF_ulSMErrLo		"default-11:ulSM:errLo"
 
+// two sided intervals strings
+#define kVDEF_intObs		"default-11:intObs:val"
+#define kVDEF_intObsErrHi	"default-11:intObs:errHi"
+#define kVDEF_intObsErrLo	"default-11:intObs:errLo"
+#define kVDEF_intSMExp		"default-11:intSM:val"
+#define kVDEF_intSMErrHi	"default-11:intSM:errHi"
+#define kVDEF_intSMErrLo	"default-11:intSM:errLo"
+
 const static float kBFBsmm = 3.2e-9;
 
 using namespace RooStats;
@@ -33,7 +43,7 @@ using namespace RooStats;
 class clsPlotter {
 	
 	public:
-	explicit clsPlotter(const char *filename) : fFilename(filename), fResultName("result_mu_s"), fSMBands(false), fDrawLegend(true), fPlotObs(true), fObs(NULL), fMgSM(NULL), fG0SM(NULL), fG1SM(NULL), fG2SM(NULL), fMgBkg(NULL), fG0Bkg(NULL), fG1Bkg(NULL), fG2Bkg(NULL) {}
+	explicit clsPlotter(const char *filename) : fFilename(filename), fResultName("result_mu_s"), fSMBands(false), fDrawLegend(true), fPlotObs(true), fUseCLs(true), fObs(NULL), fMgSM(NULL), fG0SM(NULL), fG1SM(NULL), fG2SM(NULL), fMgBkg(NULL), fG0Bkg(NULL), fG1Bkg(NULL), fG2Bkg(NULL) {}
 		
 		void plot(double cl = 0.95);
 		void print(double cl = 0.95, const char *latexName = NULL);
@@ -41,8 +51,12 @@ class clsPlotter {
 	public:
 		void setSMBands(bool smBands) { fSMBands = smBands; }
 		void setPlotObs(bool plotObs) { fPlotObs = plotObs; }
+		void setUseCLs(bool useCLs) { fUseCLs = useCLs; }
+		void setResultName(std::string resultName) { fResultName = resultName; }
 		bool getSMBands() { return fSMBands; }
 		bool getPlotObs() { return fPlotObs; }
+		bool getUseCLs() { return fUseCLs; }
+		std::string getResultName() { return fResultName; }
 	
 	private:
 		void makeObs(bool reload = false);
@@ -50,7 +64,7 @@ class clsPlotter {
 		void makeBkgBands(bool reload = false);
 		HypoTestInverterResult *loadResult(TFile *file, const char *name);
 		
-		double ulFromPoints(double testSize, vector<pair<double,double> > *vals);
+		void ulFromPoints(double testSize, vector<pair<double,double> > *vals, double *outUL, double *outLL);
 		
 	private:
 		std::string fFilename;
@@ -58,6 +72,7 @@ class clsPlotter {
 		bool fSMBands;
 		bool fDrawLegend;
 		bool fPlotObs;
+		bool fUseCLs;
 		
 		TGraph *fObs;
 		TMultiGraph *fMgSM;
@@ -71,25 +86,32 @@ class clsPlotter {
 		TGraphAsymmErrors *fG2Bkg;
 };
 
-double clsPlotter::ulFromPoints(double testSize, vector<pair<double,double> > *vals)
+void clsPlotter::ulFromPoints(double testSize, vector<pair<double,double> > *vals, double *outUL, double *outLL)
 {
 	size_t j;
 	double y1,y0;
-	sort(vals->begin(), vals->end());
 	vector<double> uls;
+	vector<double> lls;
+
+	std::sort(vals->begin(), vals->end());
 	
 	for (j = 1; j < vals->size(); j++) {
 		y1 = (*vals)[j-0].second;
 		y0 = (*vals)[j-1].second;
 		
-		if (y1 < y0) std::swap(y1,y0);
-		if (y0 <= testSize && testSize <= y1)
+		if (y1 <= testSize && testSize <= y0)
 			uls.push_back(((*vals)[j].first - (*vals)[j-1].first)/((*vals)[j].second - (*vals)[j-1].second)*(testSize - (*vals)[j-1].second) + (*vals)[j-1].first);
+		if (y0 <= testSize && testSize <= y1)
+			lls.push_back(((*vals)[j].first - (*vals)[j-1].first)/((*vals)[j].second - (*vals)[j-1].second)*(testSize - (*vals)[j-1].second) + (*vals)[j-1].first);
 	}
 	
+	// this should not be needed, but lets do it anyway
 	std::sort(uls.begin(),uls.end());
-	
-	return (uls.size() > 0 ? uls.back() : numeric_limits<double>::quiet_NaN());
+	std::sort(lls.begin(),lls.end());
+	if(outUL)
+		*outUL = (uls.size() > 0 ? uls.back() : numeric_limits<double>::quiet_NaN());
+	if(outLL)
+		*outLL = (lls.size() > 0 ? lls.front() : numeric_limits<double>::quiet_NaN());
 } // ulFromPoints()
 
 HypoTestInverterResult *clsPlotter::loadResult(TFile *file, const char *name)
@@ -100,7 +122,7 @@ HypoTestInverterResult *clsPlotter::loadResult(TFile *file, const char *name)
 		result = (HypoTestInverterResult*)wspace->obj(name);
 	}
 	
-	result->UseCLs(); // We are only working with CLs in this script
+	result->UseCLs(fUseCLs);
 	
 	return result;
 } // loadResult()
@@ -109,23 +131,41 @@ void clsPlotter::print(double cl, const char *latexName)
 {
 	TFile *file = TFile::Open(fFilename.c_str());
 	HypoTestInverterResult *resultBkg = loadResult(file, fResultName.c_str());
-	double ul, smPl,smMi;
+	double ul, ll, smPl,smMi, bf = 0;
 	double x,y;
 	double testSize = 1.0 - cl;
 	vector<pair<double,double> > vals;
-	vector<double> uls;
 	Int_t j;
 	FILE *latexFile = NULL;
 	
 	if (latexName) latexFile = fopen(latexName,"w");
 	
-	resultBkg->UseCLs();
+	resultBkg->UseCLs(fUseCLs);
 	resultBkg->SetTestSize(testSize);
 	
 	ul = resultBkg->UpperLimit();
+	ll = resultBkg->LowerLimit();
 	cout << "UL(Bs -> mumu) = " << ul*kBFBsmm << "\t(" << ul << ") @ " << (int)(cl*100.) << " % CL" << endl;
-	ul *= kBFBsmm;
-	if(latexFile) fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulObs, ul);
+	if (!fUseCLs) cout << "LL(Bs -> mumu) = " << ll*kBFBsmm << "\t(" << ll << ") @ " << (int)(cl*100.) << " % CL" << endl;
+	
+	if (!fUseCLs) {
+		// state the observed value due to the fit
+		RooWorkspace *wspace = (RooWorkspace*)file->Get("wspace");
+		if (wspace) {
+			wspace->pdf("total_pdf")->fitTo(*wspace->data("data"), RooFit::GlobalObservables(*wspace->set("nui")), RooFit::PrintLevel(-1));
+			bf = wspace->var("mu_s")->getVal();
+			cout << Form("BF(Bs -> mumu) = %e + %e - %e\t(%e + %e - %e)", bf*kBFBsmm, (ul-bf)*kBFBsmm, (bf-ll)*kBFBsmm, bf, ul-bf, bf-ll) << endl;
+		}
+	}	
+	if(latexFile) {
+		if(fUseCLs) fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulObs, ul*kBFBsmm); // print the observed upper limit
+		else {
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_intObs, bf*kBFBsmm);
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_intObsErrHi, (ul - bf)*kBFBsmm);
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_intObsErrLo, (bf - ll)*kBFBsmm);
+		}
+	}
+	
 	
 	makeObs();
 	vals.clear();
@@ -133,39 +173,47 @@ void clsPlotter::print(double cl, const char *latexName)
 		if(fObs->GetPoint(j, x, y) >= 0)
 			vals.push_back(make_pair(x,y));
 	}
-	ul = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&ul,&ll);
 	cout << "UL(Bs -> mumu) = " << ul*kBFBsmm << "\t(" << ul << ") @ " << (int)(cl * 100.) << " % CL" << endl;
+	if (!fUseCLs) cout << "LL(Bs -> mumu) = " << ll*kBFBsmm << "\t(" << ll << ") @ " << (int)(cl*100.) << " % CL" << endl;
 	
 	makeSMBands();
 	vals.clear();
 	for(j = 0; j < fG0SM->GetN(); j++) {
-		if (fG0SM->GetPoint(j,x,y) >= 0) {
+		if (fG0SM->GetPoint(j,x,y) >= 0)
 			vals.push_back(make_pair(x, y));
-		}
 	}
-	ul = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&ul,&ll);
 	
 	vals.clear();
 	for(j = 0; j < fG1SM->GetN(); j++) {
 		if (fG1SM->GetPoint(j,x,y) >= 0)
 			vals.push_back(make_pair(x, y + fG1SM->GetErrorYhigh(j)));
 	}
-	smPl = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&smPl,NULL);
 	
 	vals.clear();
 	for(j = 0; j < fG1SM->GetN(); j++) {
 		if (fG1SM->GetPoint(j,x,y) >= 0)
 			vals.push_back(make_pair(x,y - fG1SM->GetErrorYlow(j)));
 	}
-	smMi = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&smMi,NULL);
 	
-	cout << "EXP_SM[UL(Bs->mumu)] = " << ul*kBFBsmm << "+" << (smPl-ul)*kBFBsmm << "-" << (ul-smMi)*kBFBsmm << "\t(" << ul << "+" << smPl-ul << "-" << (ul-smMi) << ")" << endl;
+	if (fUseCLs)	cout << "EXP_SM[UL(Bs->mumu)] = " << ul*kBFBsmm << "+" << (smPl-ul)*kBFBsmm << "-" << (ul-smMi)*kBFBsmm << "\t(" << ul << "+" << smPl-ul << "-" << (ul-smMi) << ")" << endl;
+	else			cout << "EXP_SM[BF(Bs->mumu)] = " << kBFBsmm << "+" << (ul-1.)*kBFBsmm << "-" << (1.-ll)*kBFBsmm << "\t(" << 1. << "+" << ul-1. << "-" << 1.-ll << ")" << endl;
+	
 	if (latexFile) {
-		smPl = smPl - ul; smMi = ul - smMi;
-		ul *= kBFBsmm; smPl *= kBFBsmm; smMi *= kBFBsmm;
-		fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulSMMed, ul);
-		fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulSMErrHi, smPl);
-		fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulSMErrLo, smMi);
+		if (fUseCLs) {
+			smPl = smPl - ul; smMi = ul - smMi;
+			ul *= kBFBsmm; smPl *= kBFBsmm; smMi *= kBFBsmm;
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulSMMed, ul);
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulSMErrHi, smPl);
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulSMErrLo, smMi);
+		} else {
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_intSMExp, kBFBsmm);
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_intSMErrHi, (ul-1.)*kBFBsmm);
+			fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_intSMErrLo, (1.-ll)*kBFBsmm);
+		}
 	}
 	
 	// compute bkg expectation
@@ -176,24 +224,24 @@ void clsPlotter::print(double cl, const char *latexName)
 			vals.push_back(make_pair(x, y));
 		}
 	}
-	ul = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&ul,NULL);
 	
 	vals.clear();
 	for(j = 0; j < fG1Bkg->GetN(); j++) {
 		if (fG1Bkg->GetPoint(j,x,y) >= 0)
 			vals.push_back(make_pair(x, y + fG1Bkg->GetErrorYhigh(j)));
 	}
-	smPl = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&smPl,NULL);
 	
 	vals.clear();
 	for(j = 0; j < fG1Bkg->GetN(); j++) {
 		if (fG1Bkg->GetPoint(j,x,y) >= 0)
 			vals.push_back(make_pair(x,y - fG1Bkg->GetErrorYlow(j)));
 	}
-	smMi = ulFromPoints(testSize,&vals);
+	ulFromPoints(testSize,&vals,&smMi,NULL);
 	
-	cout << "EXP_Bkg[UL(Bs->mumu)] = " << ul*kBFBsmm << "+" << (smPl-ul)*kBFBsmm << "-" << (ul-smMi)*kBFBsmm << "\t(" << ul << "+" << smPl-ul << "-" << (ul-smMi) << ")" << endl;
-	if (latexFile) {
+	if(fUseCLs) cout << "EXP_Bkg[UL(Bs->mumu)] = " << ul*kBFBsmm << "+" << (smPl-ul)*kBFBsmm << "-" << (ul-smMi)*kBFBsmm << "\t(" << ul << "+" << smPl-ul << "-" << (ul-smMi) << ")" << endl;
+	if (latexFile && fUseCLs) {
 		smPl = smPl - ul; smMi = ul - smMi;
 		ul *= kBFBsmm; smPl *= kBFBsmm; smMi *= kBFBsmm;
 		fprintf(latexFile, "\\vdef{%s}	{\\ensuremath{{%e}}}\n", kVDEF_ulBkgMed, ul);
@@ -210,8 +258,9 @@ void clsPlotter::makeObs(bool reload)
 	TFile f(fFilename.c_str());
 	HypoTestInverterResult *result = loadResult(&f,fResultName.c_str());
 	int j,size = result->ArraySize();
+	std::vector<double> xvalues;
 	std::vector<unsigned int> index(size);
-	double cls;
+	double clevel;
 	
 	if (fObs && reload) {
 		delete fObs;
@@ -221,16 +270,17 @@ void clsPlotter::makeObs(bool reload)
 	if (!fObs) {
 		fObs = new TGraph;
 		
-		fObs->SetTitle("CLs observed");
+		fObs->SetTitle(Form("%s observed", (fUseCLs ? "CLs" : "CLs+b")));
 		fObs->SetLineWidth(2);
 		
 		// sequential access
-		TMath::SortItr(result->fXValues.begin(), result->fXValues.end(), index.begin(), false);
+		for (j = 0; j < result->ArraySize(); j++) xvalues.push_back(result->GetXValue(j));
+		TMath::SortItr(xvalues.begin(), xvalues.end(), index.begin(), false);
 		
 		for (j = 0; j < size; j++) {
-		  cls = result->CLs(index[j]);
-		  if (cls < 0) cls = 1.0;
-		  fObs->SetPoint(j, result->GetXValue(index[j]), cls);
+		  clevel = fUseCLs ? result->CLs(index[j]) : result->CLsplusb(index[j]);
+		  if (clevel < 0) clevel = 1.0;
+		  fObs->SetPoint(j, result->GetXValue(index[j]), clevel);
 		}
 	}
 } // makeObs()
@@ -248,7 +298,9 @@ void clsPlotter::makeSMBands(bool reload)
 	double q[5];
 	double *x;
 	unsigned int j,k,ixBkg,ixSM;
-	vector<double> cls;
+	vector<double> pvalues;
+	vector<double> xvaluesBkg;
+	vector<double> xvaluesSM;
 	
 	if (reload) {
 		if(fMgSM) {
@@ -279,13 +331,15 @@ void clsPlotter::makeSMBands(bool reload)
 	fG1SM = new TGraphAsymmErrors;
 	fG2SM = new TGraphAsymmErrors;
 	
-	fG0SM->SetTitle("Expected SM CLs - Median");
-	fG1SM->SetTitle("Expected SM CLs #pm 1 #sigma");
-	fG2SM->SetTitle("Expected SM CLs #pm 2 #sigma");
+	fG0SM->SetTitle(Form("Expected SM %s - Median",(fUseCLs ? "CLs" : "CLs+b")));
+	fG1SM->SetTitle(Form("Expected SM %s #pm 1 #sigma",(fUseCLs ? "CLs" : "CLs+b")));
+	fG2SM->SetTitle(Form("Expected SM %s #pm 2 #sigma",(fUseCLs ? "CLs" : "CLs+b")));
 	
 	// sequential access
-	TMath::SortItr(resultBkg->fXValues.begin(), resultBkg->fXValues.end(), indexBkg.begin(), false);
-	TMath::SortItr(resultSM->fXValues.begin(), resultSM->fXValues.end(), indexSM.begin(), false);
+	for (j = 0; j < (unsigned int)resultBkg->ArraySize(); j++) xvaluesBkg.push_back(resultBkg->GetXValue(j));
+	for (j = 0; j < (unsigned int)resultSM->ArraySize(); j++) xvaluesSM.push_back(resultSM->GetXValue(j));
+	TMath::SortItr(xvaluesBkg.begin(), xvaluesBkg.end(), indexBkg.begin(), false);
+	TMath::SortItr(xvaluesSM.begin(), xvaluesSM.end(), indexSM.begin(), false);
 	
 	p[0] = ROOT::Math::normal_cdf(-2);
 	p[1] = ROOT::Math::normal_cdf(-1);
@@ -315,14 +369,14 @@ void clsPlotter::makeSMBands(bool reload)
 		// SM distribution saved in background
 		dist = hypoSM->GetBackGroundIsAlt() ? hypoSM->GetAltDistribution() : hypoSM->GetNullDistribution();
 		const vector<Double_t> &vec = dist->GetSamplingDistribution();
-		cls.clear();
+		pvalues.clear();
 		for (k = 0; k < vec.size(); k++) {
 			tempResult.SetTestStatisticData(vec[k]);
-			cls.push_back(tempResult.CLs());
+			pvalues.push_back(fUseCLs ? tempResult.CLs() : tempResult.CLsplusb());
 		}
 		
-		x = const_cast<double*>(&cls[0]);
-		TMath::Quantiles(cls.size(), 5, x, q, p, false);
+		x = const_cast<double*>(&pvalues[0]);
+		TMath::Quantiles(pvalues.size(), 5, x, q, p, false);
 		
 		fG0SM->SetPoint(j, resultBkg->GetXValue(ixBkg), q[2]);
 		
@@ -354,6 +408,7 @@ void clsPlotter::makeBkgBands(bool reload)
 	SamplingDistribution *dist;
 	unsigned int j,ix,nbr = result->ArraySize();
 	std::vector<unsigned int> index(nbr);
+	std::vector<double> xvalues;
 	double p[5];
 	double q[5];
 	double *x;
@@ -387,18 +442,20 @@ void clsPlotter::makeBkgBands(bool reload)
 	fG1Bkg = new TGraphAsymmErrors;
 	fG2Bkg = new TGraphAsymmErrors;
 	
-	fG0Bkg->SetTitle("Expected CLs - Median");
-	fG1Bkg->SetTitle("Expected CLs #pm 1 #sigma");
-	fG2Bkg->SetTitle("Expected CLs #pm 2 #sigma");
+	fG0Bkg->SetTitle(Form("Expected %s - Median", (fUseCLs ? "CLs" : "CLs+b")));
+	fG1Bkg->SetTitle(Form("Expected %s #pm 1 #sigma", (fUseCLs ? "CLs" : "CLs+b")));
+	fG2Bkg->SetTitle(Form("Expected %s #pm 2 #sigma", (fUseCLs ? "CLs" : "CLs+b")));
 	
 	// seq access
-	TMath::SortItr(result->fXValues.begin(), result->fXValues.end(), index.begin(), false);
+	for (j = 0; j < (unsigned int)result->ArraySize(); j++) xvalues.push_back(result->GetXValue(j));
+	TMath::SortItr(xvalues.begin(), xvalues.end(), index.begin(), false);
 	p[0] = ROOT::Math::normal_cdf(-2);
 	p[1] = ROOT::Math::normal_cdf(-1);
 	p[2] = 0.5;
 	p[3] = ROOT::Math::normal_cdf(1);
 	p[4] = ROOT::Math::normal_cdf(2);
 	
+	result->UseCLs(fUseCLs);
 	for (j = 0; j < nbr; j++) {
 		ix = index[j];
 		
@@ -465,7 +522,7 @@ void clsPlotter::plot(double cl)
 	if (fPlotObs) {
 		fObs->Draw("AL");
 		fObs->GetXaxis()->SetTitle("BF / BF_{SM}");
-		fObs->GetYaxis()->SetTitle("CL_{s}");
+		fObs->GetYaxis()->SetTitle(Form("CL_{%s}", (fUseCLs ? "s" : "s+b")));
 		fObs->GetYaxis()->SetTitleOffset(1.0);
 	}
 	mg->Draw( (fPlotObs ? "" : "A") );
@@ -479,14 +536,30 @@ void clsPlotter::plot(double cl)
 	
 	// draw legend
 	leg->Draw();
+	
+	gPad->RedrawAxis();
 } // plot()
 
-void plotResult(const char *file, bool SMExp, const char *latexName = NULL, bool plotObs = true, double cl = 0.95)
+void plotCLs(const char *file, bool SMExp, const char *latexName = NULL, bool plotObs = true, double cl = 0.95)
 {
 	clsPlotter plot(file);
 	
+	plot.setResultName("Hybrid_Ul_mu_s");
+	plot.setUseCLs(true);
 	plot.setPlotObs(plotObs);
 	plot.setSMBands(SMExp);
 	plot.plot(cl);
 	plot.print(cl, latexName);
-} // plotResult()
+} // plotCLs()
+
+void plotTwoSided(const char *file, bool SMExp, const char *latexName =  NULL, bool plotObs = true, double cl = 0.68)
+{
+	clsPlotter plot(file);
+	
+	plot.setResultName("Hybrid_Int_mu_s");
+	plot.setUseCLs(false);
+	plot.setPlotObs(plotObs);
+	plot.setSMBands(SMExp);
+	plot.plot(cl);
+	plot.print(cl, latexName);
+} // plotTwoSided()
