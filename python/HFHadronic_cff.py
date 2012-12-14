8a1e4045fb773d61643387d4896bce99b53fdedd
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2012-12-14 16:56:49 +0000

    hadronic physics

---
+++ python/HFHadronic_cff.py
@@ -0,0 +1,58 @@
+import FWCore.ParameterSet.Config as cms
+
+# ----------------------------------------------------------------------
+dstarpiDump = cms.EDAnalyzer(
+    "HFBd2DstarPi",
+    verbose            = cms.untracked.int32(0), 
+    tracksLabel        = cms.untracked.InputTag('generalTracks'),
+    PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
+    trackPt            = cms.untracked.double(3.5),
+    slowPionPt         = cms.untracked.double(0.2),
+    deltaM             = cms.untracked.double(0.02),
+    D0Window           = cms.untracked.double(0.1),
+    deltaR             = cms.untracked.double(99.0),
+    maxDoca            = cms.untracked.double(0.1),
+    maxD0              = cms.untracked.double(2.0),
+    maxDz              = cms.untracked.double(99.0)
+    )
+
+
+# ----------------------------------------------------------------------
+dstarDump = cms.EDAnalyzer(
+    "HFDstar",
+    verbose            = cms.untracked.int32(0), 
+    tracksLabel        = cms.untracked.InputTag('generalTracks'),
+    PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
+    trackPt            = cms.untracked.double(3.5),
+    slowPionPt         = cms.untracked.double(0.2),
+    D0Window           = cms.untracked.double(0.1),
+    deltaM             = cms.untracked.double(0.03),
+    deltaR             = cms.untracked.double(0.3),
+    maxDoca            = cms.untracked.double(0.1),
+    maxD0              = cms.untracked.double(2.0),
+    maxDz              = cms.untracked.double(99.0)
+    )
+
+# ----------------------------------------------------------------------
+hhDump = cms.EDAnalyzer(
+    "HFDiTracks",
+    verbose            = cms.untracked.int32(0), 
+    tracksLabel        = cms.untracked.InputTag("generalTracks"),
+    PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
+    trackPt            = cms.untracked.double(4.0),
+    track1Mass         = cms.untracked.double(0.1057),
+    track2Mass         = cms.untracked.double(0.1057),
+    massLow            = cms.untracked.double(4.5),
+    massHigh           = cms.untracked.double(6.5),
+    maxDoca            = cms.untracked.double(0.025),
+    pvWeight           = cms.untracked.double(0.70),
+    type               = cms.untracked.int32(211211),
+    sameSign           = cms.untracked.bool( False )
+    )
+   
+# ######################################################################
+# Sequences
+# ######################################################################
+hadronicSequence     = cms.Sequence(dstarpiDump
+                                    *dstarDump
+                                    *hhDump)
---
+++ test/v13/bmm-hadronic-2012-XXXX.py
@@ -0,0 +1,51 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HFA")
+
+# ----------------------------------------------------------------------
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+
+# ----------------------------------------------------------------------
+# -- Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "GR_P_V40::All"
+
+# ----------------------------------------------------------------------
+# -- Input files
+POOLSOURCE
+
+# ----------------------------------------------------------------------
+rootFileName = "bmm-hadronic-2012-XXXX.root"
+
+
+process.tree = cms.EDAnalyzer(
+    "HFTree",
+    verbose      = cms.untracked.int32(0),
+    requireCand  =  cms.untracked.bool(True),
+    fileName     = cms.untracked.string(rootFileName)
+    )
+
+# ----------------------------------------------------------------------
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFRecoStuff_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFHadronic_cff")
+process.load("HeavyFlavorAnalysis.Bs2MuMu.HFPhysicsDeclared_cff")
+
+# ----------------------------------------------------------------------
+process.p = cms.Path(
+    process.recoStuffSequence*
+    process.hadronicSequence*
+    process.tree
+)
