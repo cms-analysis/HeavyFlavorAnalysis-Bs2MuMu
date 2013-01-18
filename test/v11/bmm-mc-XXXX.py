56ccd6c1b06e33a522791dad6d80facecaead23d
Jose Lazo-Flores <sha1-91a8bfb0bc6cbd83cde7fbdf4e103162926f4130@cern.ch>
2013-01-18 13:51:36 +0000

    Changed the global tag.

--- test/v11/bmm-mc-XXXX.py
+++ test/v11/bmm-mc-XXXX.py
@@ -20,7 +20,7 @@ process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "START42_V17::All"
+process.GlobalTag.globaltag = "START44_V10::All"
 
 # ----------------------------------------------------------------------
 # POOLSOURCE
