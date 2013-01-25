d2d67a1f382b0260ccf9a8f73d2f25630b443618
Christoph Nageli <sha1-067387c955e77eaf9ce2d72cbd8fd47dbe7f524e@cern.ch>
2013-01-25 11:27:57 +0000

    adapt changes to HFDumpMuons / HFDumpTracks in the python configuration

--- python/HFRecoStuff_cff.py
+++ python/HFRecoStuff_cff.py
@@ -25,17 +25,25 @@ trkDump = cms.EDAnalyzer(
     associatorLabel                = cms.untracked.InputTag('TrackAssociatorByHits'),
     doTruthMatching                = cms.untracked.int32(3),
     simTracksLabel                 = cms.untracked.InputTag('allLayer1TrackCands'),
-    loadCalomuons                  = cms.untracked.bool(False)
+    loadCalomuons                  = cms.untracked.bool(False),
+	propMuon					   = cms.PSet(
+		useStation2 = cms.bool(False), 
+        useTrack = cms.string("tracker"),
+        useState = cms.string("atVertex"),  # in AOD
+        useSimpleGeometry = cms.bool(True), # use just one cylinder and two planes, not all the fancy chambers
+        )
     )
 
 # ----------------------------------------------------------------------
 muonDump = cms.EDAnalyzer(
     "HFDumpMuons",
     verbose         = cms.untracked.int32(0),
+	tracksLabel     = cms.untracked.InputTag("generalTracks"),
     muonsLabel      = cms.untracked.InputTag("muons"),
     calomuonsLabel  = cms.untracked.InputTag("calomuons"),
     doTruthMatching = cms.untracked.int32(0),
     runOnAOD        = cms.untracked.bool(True),
+	maxTrackDist    = cms.untracked.double(0.2),
     # Configuration for the extrapolation at the muon system 
     propM1 = cms.PSet(
                       useStation2 = cms.bool(False), 
@@ -50,7 +58,6 @@ muonDump = cms.EDAnalyzer(
                       useSimpleGeometry = cms.bool(True), # use just one cylinder and two planes, not all the fancy chambers
                       )
     )
-
 
 # ----------------------------------------------------------------------
 triggerDump = cms.EDAnalyzer(
