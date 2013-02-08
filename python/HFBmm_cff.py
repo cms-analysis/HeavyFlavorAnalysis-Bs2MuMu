7a36528e0ef0374868faf6b2f4b21543d7e80892
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2013-02-08 16:14:11 +0000

    remove hh from default sequence

--- python/HFBmm_cff.py
+++ python/HFBmm_cff.py
@@ -61,28 +61,8 @@ bspsiphiDump = cms.EDAnalyzer(
     type               = cms.untracked.int32(531)
     )
 
-# ----------------------------------------------------------------------
-hhDump = cms.EDAnalyzer(
-    "HFDiTracks",
-    verbose            = cms.untracked.int32(0), 
-    tracksLabel        = cms.untracked.InputTag("generalTracks"),
-    PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
-    BeamSpotLabel      = cms.untracked.InputTag("offlineBeamSpot"),
-    trackPt            = cms.untracked.double(4.0),
-    track1Mass         = cms.untracked.double(0.1057),
-    track2Mass         = cms.untracked.double(0.1057),
-    massLow            = cms.untracked.double(4.5),
-    massHigh           = cms.untracked.double(6.5),
-    maxDoca            = cms.untracked.double(0.025),
-    pvWeight           = cms.untracked.double(0.70),
-    type              = cms.untracked.int32(211211),
-    nbrMuons           = cms.untracked.int32(0),
-    closeToMuons       = cms.untracked.bool(False)
-    )
-
-
 
 # ######################################################################
 # Sequences
 # ######################################################################
-bmmSequence     = cms.Sequence(bmmDump*bupsikpDump*bspsiphiDump*hhDump)
+bmmSequence     = cms.Sequence(bmmDump*bupsikpDump*bspsiphiDump)
