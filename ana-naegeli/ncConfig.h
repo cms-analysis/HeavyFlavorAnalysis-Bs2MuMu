c5ec2d7a8ba26677308ec5687203c4521a91f754
Christoph Nageli <sha1-067387c955e77eaf9ce2d72cbd8fd47dbe7f524e@cern.ch>
2013-02-11 13:27:46 +0000

    - updated config class for my analysis. - use xml configuration file

--- ana-naegeli/ncConfig.h
+++ ana-naegeli/ncConfig.h
@@ -9,26 +9,40 @@
 #define NCCONFIG_H
 
 #include <string>
-
 #include <TCut.h>
+
+#ifndef __CINT__
+#include <libxml/parser.h>
+#endif
 
 class ncConfig {
 	
 	public:
 		ncConfig();
 		
+#ifndef __CINT__
+		ncConfig(xmlNodePtr node);
+#endif
+		
 		const char *getDataFile() { return fDataFile.c_str(); }
 		const char *getMCFile() { return fMCFile.c_str(); }
 		const char *getVarFile() { return fVarFile.c_str(); }
 		const char *getStyle() { return fStyle.c_str(); }
 		TCut getPreselection() { return fCutPreselection; }
-	
+		TCut getMCSelection() { return fMCSelection; }
+		TCut getDataSelection() { return fDataSelection; }
+		
+		void dump();
+		
 	private:
 		std::string fDataFile;
+		std::string fAccFile;
 		std::string fMCFile;
 		std::string fVarFile;
 		std::string fStyle;
 		TCut fCutPreselection;
+		TCut fMCSelection;
+		TCut fDataSelection;
 };
 
 #endif
